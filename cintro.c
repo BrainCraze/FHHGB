4beeae1092212f1c3f4da574423126e334128fd4
root <root@localhost.localdomain>
2020-09-19 10:37:41 +0200

    first lesson  Bitte geben Sie eine Commit-Beschreibung für Ihre Änderungen ein. Zeilen,

---
+++ cintro.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+
+//int main (int argc, char** argv)
+int main (int argc, char* argv[]){
+    int a = 3, b = 3;
+
+    if (a == b)
+    {
+        printf("equal\n");
+    }
+    else
+    {
+        printf("unequal\n");
+    }
+    
+
+    return 0;
+}
