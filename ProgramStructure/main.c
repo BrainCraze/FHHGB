85f17c2d650db79d6f83dc85a9e1b76d078af012
root <root@localhost.localdomain>
2020-09-26 11:08:21 +0200

    sdikasdbkh

--- ProgramStructure/main.c
+++ ProgramStructure/main.c
@@ -1,7 +1,24 @@
 #include <stdio.h>
 #include "lib.h"
 
+void swap(int a, int b){
+    a = b - a;
+    b = b - a;
+    a = a + b;
+    printf("swapped: a = %d, b = %d\n", a, b);
+}
+
+void swap(int* a, int*b){
+    //des ganze implementiert mit zeiger und temp variable
+}
+
 int main (void){
+
+    int a = 1;
+    int b = 6;
+
     printf("sum ? %d\n", sum(3,5));
+    printf("übergeben an swap: a = %d, b = %d\n", a, b);
+    swap(a, b);
     return 0;
 }
