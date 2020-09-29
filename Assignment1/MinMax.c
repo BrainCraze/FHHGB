99f2ff31495b5fd616c08958cab826017ae65959
t.ringer <t.ringer@gmx.net>
2020-09-29 10:30:05 +0200

    fixed errors on input in assignment minmax

--- Assignment1/MinMax.c
+++ Assignment1/MinMax.c
@@ -11,11 +11,16 @@ int main (int argc, char* argv[]){
     for (int i = 1; i < argc; i++) //starts at 1 because the first argument (argv[0]) ist he program name itself (./minmax) so the first real argument is argv[1].
     {
         errno = 0;
-        number_to_compare = strtol(argv[i], NULL, 10);
+        char * end;
+        number_to_compare = strtol(argv[i], &end, 10);
         
         if ((errno == ERANGE && (number_to_compare == LONG_MAX || number_to_compare == LONG_MIN)) || (errno != 0 && number_to_compare == 0))
         {
             perror("strtol");
+            exit(EXIT_FAILURE);
+        }
+        if (argv[i] == end) {
+            printf("No Valid numbers found\n");
             exit(EXIT_FAILURE);
         }
         
